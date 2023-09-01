98cd55a3943893c66f0ce2ef9a3c28339d1aa15a
Bálint Ducsai <balint.ducsai@gmail.com>
2023-09-02 00:06:46 +0200

    feat: init next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid h-screen place-items-center">
+      <h1 className='font-bold text-5xl text-center'>Hello papirsarkany@next! 🎉</h1>
+    </div>
+  )
+}
