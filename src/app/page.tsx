33c8373c367220fbca4750d0c5b71c6b08f786c9
Bálint Ducsai <balint.ducsai@gmail.com>
2023-09-02 13:10:30 +0200

    feat: add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Logo from '@/assets/logo.svg';
+
 export default function Home() {
-  return <h1 className="font-bold text-5xl text-center">Hello papirsarkany@next! 🎉</h1>;
+  return (
+    <>
+      <Logo className="w-[368px]" />
+      <h1 className="font-bold text-5xl text-center">Hello papirsarkany@next! 🎉</h1>;
+    </>
+  );
 }
