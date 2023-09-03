9549cf03e67270cc809783670fad3d7077fdad5b
Bálint Ducsai <balint.ducsai@gmail.com>
2023-09-03 21:55:40 +0200

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Logo from '@/assets/logo.svg';
+import Card from '@/components/Card';
 
 export default function Home() {
   return (
     <>
-      <Logo className="w-[368px]" />
-      <h1 className="font-bold text-5xl text-center">Hello papirsarkany@next! 🎉</h1>;
+      <Logo className="w-full px-12 sm:w-[368px] " />
+
+      <h1 className="font-bold text-5xl text-center">Hello papirsarkany@next! 🎉</h1>
+
+      <Card>Hello Card!</Card>
     </>
   );
 }
