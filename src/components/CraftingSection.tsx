11444ea551056360295862115c64843b372b424f
Bálint Ducsai <balint.ducsai@gmail.com>
2023-09-23 23:37:26 +0200

    feat: add home page sections

---
+++ src/components/CraftingSection.tsx
@@ -0,0 +1,26 @@
+import CraftingIcon from '@/assets/crafting.svg';
+
+export default function CraftingSection() {
+  return (
+    <section id="sarkany-keszites" className=" py-12">
+      <div className="pl-4 pt-4 sm:float-right">
+        <CraftingIcon className="h-32 w-32" />
+      </div>
+      <div>
+        <h2 className='font-bold'>Sárkányépítő foglalkozások</h2>
+
+        <p>
+          A sárkánykészítés és -repítés régebben mindennapos dolog volt a gyerekek hétköznapjaiban. Ez a &quot;tudomány&quot;
+          apáról fiúra szállt, illetve a nagyobbaktól lesték el a kisebbek. A gyerekek mára elfelejtették ezt a szép
+          régi játékot.
+        </p>
+
+        <p>
+          Vállalom sárkányépítő foglalkozások vezetését gyerekrendezvények, -intézmények és céges rendezvények számára.
+        </p>
+
+        <p>Anyagot biztosítok. (Szelet a megrendelő biztosít.) Ár egyedi megállapodás alapján.</p>
+      </div>
+    </section>
+  );
+}
