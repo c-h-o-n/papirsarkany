a36bd913b80592f4f55e662fd9a7a1b687a7efc3
chon <balint.ducsai@gmail.com>
2023-09-23 09:55:13 +0200

    feat: add prisma client

--- src/app/sarkanyok/[slug]/page.tsx
+++ src/app/sarkanyok/[slug]/page.tsx
@@ -1,3 +1,19 @@
-export default function Kite({ params }: { params: { slug: string } }) {
-  return <div className='text-3xl h-full grid place-items-center'>Post: {params.slug}</div>
+import { getKitebySlug, getKites } from '@/lib/db';
+
+export async function generateStaticParams() {
+  const kites = await getKites();
+
+  return kites.map((kite) => ({
+    slug: kite.slug,
+  }));
+}
+
+export default async function Kite({ params }: { params: { slug: string } }) {
+  const kite = await getKitebySlug(params.slug);
+  
+  return (
+    <div className="grid h-full place-items-center text-3xl">
+      Post: {params.slug} <br /> {kite && kite.name}
+    </div>
+  );
 }
