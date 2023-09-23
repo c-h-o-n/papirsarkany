19bbdee2e199c0017d2fb5ffc27762a9d7e95a07
chon <balint.ducsai@gmail.com>
2023-09-23 23:37:26 +0200

    feat: add home page sections

--- src/components/AboutBusinessSection.tsx
+++ src/components/AboutBusinessSection.tsx
@@ -2,25 +2,52 @@ import AboutBusinessIcon from '@/assets/about-business.svg';
 
 export default function AboutBusinessSection() {
   return (
-    <section id="vallalkozas" className="grid grid-cols-2">
-      <AboutBusinessIcon className="h-32 w-32" />
+    <section id="vallalkozas" className="py-12">
+      <div className="pb-4 pr-4 sm:float-left">
+        <AboutBusinessIcon className="h-32 w-32" />
+      </div>
       <div>
-        AboutBusinessSection
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, accusantium? Autem aperiam culpa modi consequuntur est sed ad necessitatibus saepe amet? Praesentium dolores dolorem dignissimos, ratione mollitia quod accusamus ut!
-        Nam, dolor dolorum corrupti quasi hic amet officia, culpa dicta ratione velit aliquid. Provident quam reiciendis ea aliquam consequatur hic sint ut esse, delectus deserunt, qui fuga facere laudantium corrupti.
-        Quaerat ad placeat perspiciatis. Dolores laborum voluptates aspernatur! Aperiam, porro maiores laudantium voluptas excepturi magnam beatae blanditiis reprehenderit praesentium molestiae? Quasi dolorem rem molestias vel optio quo iure, eum harum.
-        Cumque debitis, recusandae, consequatur cum a sequi quae inventore eaque consectetur aspernatur quam dolorem deserunt ut hic provident autem impedit excepturi voluptatem dicta earum! Facilis autem quisquam porro cumque repudiandae.
-        Ullam animi in laboriosam repellendus, ipsum magnam. Distinctio similique eligendi cupiditate modi labore reiciendis accusamus voluptatem, ullam nostrum, incidunt veritatis quod aliquam illum consequuntur eius? Blanditiis beatae suscipit neque! Dolor!
-        Aliquid, molestias harum quo neque adipisci repudiandae libero beatae doloribus enim corrupti totam, et sed soluta voluptatibus, quaerat obcaecati qui aspernatur recusandae officia quis cum porro deserunt assumenda. Qui, totam.
-        Laboriosam iure minus vel explicabo! Voluptates, dolor. Distinctio temporibus sint eum perferendis quia, ex expedita ad accusamus quasi itaque nesciunt ratione odio porro in culpa magni quod suscipit iusto velit?
-        Ad molestias doloremque obcaecati illum tempore. Quam doloribus rem distinctio deleniti iure eum fuga architecto fugiat itaque aspernatur asperiores animi vel quasi, aperiam qui ut ad, ex nisi cum quia?
-        Atque mollitia inventore dignissimos illo sed quaerat voluptatum officiis maiores fuga corrupti, laboriosam impedit esse cum tempore repellendus possimus sit explicabo officia alias sunt asperiores labore accusamus? In, ipsa blanditiis!
-        Beatae temporibus quis ut animi iusto fuga veniam minus ipsa, nobis modi optio nostrum, placeat vitae repellat adipisci laboriosam delectus magni excepturi sapiente. Commodi error et natus saepe pariatur recusandae.
-        Aliquam ex, quas nostrum rem aspernatur libero at eaque quis sit similique impedit, consequuntur doloremque excepturi optio ullam minus, magnam nemo non nam ipsum veritatis reprehenderit ab. Blanditiis, rerum perferendis.
-        Deserunt ex tempore consequuntur repellat dignissimos earum voluptatum asperiores. Praesentium repellat nemo reprehenderit, doloremque totam tempore voluptates laborum libero iure consequatur inventore rerum vel omnis sed adipisci porro commodi sequi!
-        Laboriosam veniam quaerat illo ratione sint consectetur assumenda id provident doloremque est cum, architecto magni? Quam ad voluptate excepturi sequi. Sed maiores debitis eius ex culpa laboriosam expedita recusandae quasi.
-        Aut velit veritatis sed impedit inventore deleniti odio, nisi dicta molestiae tempora magnam omnis dolore assumenda quae veniam optio soluta harum iusto! Eum ducimus quia voluptate quos nobis autem assumenda.
-        Velit, eveniet ducimus dolore vitae laborum repellat enim? Sint quis ipsum impedit nisi nesciunt, mollitia recusandae ad omnis? Esse officiis pariatur a fugiat quos non facere est reprehenderit, quis porro.
+        <p>Üzletem nincs, ezért postai utánvétellel szállítok, melynek költsége a megrendelőt terheli.</p>
+
+        <p>
+          A postai utánvételes szállításhoz szükség van a megrendelő pontos nevére, címére (irányítószámmal), továbbá a
+          rendelt sárkány nevére, mennyiségére. Ha a számlázási cím nem azonos a megrendelőével, kérem azt is
+          pontosanmegjelölni!
+        </p>
+
+        <p>Személyesen Nagykovácsiban (63-as BKV busszal megközelíthető) is vásárolhat.</p>
+
+        <p>
+          Sárkányaim repülési és nyolc napos pénzvisszafizetési garanciával kaphatók. A visszaküldés költsége a vevőt
+          terheli.
+        </p>
+
+        <p>
+          Minden érdeklődőnek egy óra ingyenes oktatás sárkány biztosításával - vásárlási kötelezettség nélkül -
+          Nagykovácsiban. (Telefonos időpont egyeztetés után.)
+        </p>
+
+        <p>
+          Mindenkinek kellemes sárkányeresztést kíván a www.papirsarkany.hu egyéni vállalkozás tulajdonosa: <br />
+          <div>
+          Ducsai Barnabás
+          <br />
+          <b>Adószám:</b> 61090938-1-33
+          <br />
+          <div className="gap-1 sm:flex">
+            <b>Számlaszámom:</b>
+            <div>
+              www.papirsarkany.hu
+              <br />
+              Ducsai Barnabás
+              <br />
+              Erste Bank 11600006-00000000-76709302
+            </div>
+          </div>
+        </div>
+        </p>
+
+
       </div>
     </section>
   );
---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,22 @@
+import ContactIcon from '@/assets/contact.svg';
+
+export default function ContactSection() {
+  return (
+    <section id="elerhetoseg" className=" py-12">
+      <div className="pl-4 pt-4 sm:float-left">
+        <ContactIcon className="h-32 w-32" />
+      </div>
+      <div>
+        <h2 className="font-bold">Elérhetőség</h2>
+        <b>Email:</b> mail@papirsarkany.hu
+        <br />
+        <b>Telefon:</b> 0630 9754 786
+        <iframe
+          title="terkep"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1131.6105816797208!2d18.87852618960264!3d47.57870829362802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xceefc53f4870d42e!2swww.papirsarkany.hu!5e0!3m2!1shu!2shu!4v1579197549648!5m2!1shu!2shu"
+          className="h-[600px] w-full rounded-3xl"
+        />
+      </div>
+    </section>
+  );
+}
