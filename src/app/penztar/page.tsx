31b03d17de2a30e326c9c14f30277c031aefe0e0
chon <balint.ducsai@gmail.com>
2023-09-26 23:09:23 +0200

    feat: add cart function

---
+++ src/app/penztar/page.tsx
@@ -0,0 +1,8 @@
+export default function Checkout() {
+  return (
+    <div className="container p-8">
+      <h1 className='text-center'>Fizetés</h1>
+      <h2 className="underline underline-offset-8">Szállítás</h2>
+    </div>
+  );
+}
