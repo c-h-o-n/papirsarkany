ce67a85a0f0731444a34eb8413e1a7fe9f22b71e
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2023-09-26 23:40:51 +0200

    fix: hydration error

--- src/app/kosar/page.tsx
+++ src/app/kosar/page.tsx
@@ -1,18 +1,16 @@
 import CheckoutLink from '@/components/CheckoutLink';
 import OrderSummaryCard from '@/components/OrderSummaryCard';
-import Link from 'next/link';
 
 export default function Cart() {
   return (
-    <div className="container flex flex-col p-8 gap-4">
+    <div className="container flex flex-col gap-4 p-8">
       <h1 className="text-center text-5xl font-bold">Kosár tartalma</h1>
 
-<div className="flex-1">
-      <OrderSummaryCard />
+      <div className="flex-1">
+        <OrderSummaryCard />
+      </div>
 
-</div>
-
-    <CheckoutLink />
+      <CheckoutLink />
     </div>
   );
 }
