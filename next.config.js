e6295cb8254c3e25659cb071f157e968d8a6ee13
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2023-11-01 13:29:45 +0100

    feat: add version number to layout

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+const fs = require('fs');
+
+function getAppVersion() {
+  const { version } = JSON.parse(fs.readFileSync('./package.json'));
+  return version;
+}
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ['@svgr/webpack'],
     });
 
     return config;
-  }
-}
+  },
+  publicRuntimeConfig: {
+    appVersion: getAppVersion(),
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
