da9d462d6d3437d842b8acf5524b04cc3a158ed5
chon <balint.ducsai@gmail.com>
2023-11-01 12:25:02 +0100

    feat: add custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="grid place-items-center">
+      <div className="text-center">
+        <h2>A keresett oldal nem található.</h2>
+        <Link className="d-btn d-btn-ghost d-btn-lg normal-case" href="/sarkanyok">
+          Nézz szét a sárkányaink közt
+        </Link>
+      </div>
+    </div>
+  );
+}
