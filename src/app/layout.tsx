69ff40b5174eb673f68378eb538666d798ce2f29
chon <balint.ducsai@gmail.com>
2023-11-01 13:29:45 +0100

    feat: add version number to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import getConfig from 'next/config';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,9 +13,13 @@ export const metadata: Metadata = {
   description: 'Papírsárkány árusítás 1984-óta.',
 };
 
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const { publicRuntimeConfig } = getConfig()
+
   return (
-    <html lang="en" className="scroll-pt-[68px] scroll-smooth sm:scroll-pt-[72px]">
+    <html lang="en" className="scroll-pt-[68px] scroll-smooth sm:scroll-pt-[72px]" data-app-version={publicRuntimeConfig.appVersion}>
       <body className={`${inter.className} `}>
         <div className="flex min-h-screen flex-col">
           <Navbar />
