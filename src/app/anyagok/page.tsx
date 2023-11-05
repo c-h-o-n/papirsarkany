1b08b1353bd134e112a114ae9ceffc72db08df21
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2023-11-05 21:45:36 +0100

    feat: add reels

--- src/app/anyagok/page.tsx
+++ src/app/anyagok/page.tsx
@@ -1,28 +1,45 @@
 import MasonryContainer from '@/components/MasonryContainer';
+import ReelCard from '@/components/ReelCard';
 import RodCard from '@/components/RodCard';
-import { getRods } from '@/lib/db';
+import { getReels, getRods } from '@/lib/db';
 import { ProductCategoryMap } from '@/lib/formatters';
 
 export default async function Materials() {
+  const reels = await getReels({ orderBy: { price: 'asc' } });
   const rods = await getRods({ orderBy: { price: 'asc' } });
   const pipes = [];
   const lines = [];
-  const handles = [];
+
+  console.log(reels);
 
   return (
     <div className="container p-8">
       <h1 className="text-center font-bold">Anyagok</h1>
 
-      <h2 className="font-bold">{rods.length > 0 && ProductCategoryMap['PalcakRudak']}</h2>
-      <MasonryContainer>
-        {rods.map((rod) => (
-          <RodCard rod={rod} key={rod.id} />
-        ))}
-      </MasonryContainer>
+      {reels && reels.length > 0 && (
+        <>
+          <h2 className="font-bold">{ProductCategoryMap['Zsinortartok']}</h2>
+          <MasonryContainer>
+            {reels.map((reel) => (
+              <ReelCard reel={reel} key={reel.id} />
+            ))}
+          </MasonryContainer>
+        </>
+      )}
+      
+      {rods && rods.length && (
+        <>
+          <h2 className="font-bold">{ProductCategoryMap['PalcakRudak']}</h2>
+          <MasonryContainer>
+            {rods.map((rod) => (
+              <RodCard rod={rod} key={rod.id} />
+            ))}
+          </MasonryContainer>
+        </>
+      )}
 
       <h2 className="font-bold">{pipes.length > 0 && ProductCategoryMap['Csovek']}</h2>
       <h2 className="font-bold">{lines.length > 0 && ProductCategoryMap['Zsinorok']}</h2>
-      <h2 className="font-bold">{handles.length > 0 && ProductCategoryMap['Zsinortartok']}</h2>
     </div>
   );
 }
