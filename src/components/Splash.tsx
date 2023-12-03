b80e17d1e3103a76c9517a2c72edaae310a85a13
chon <balint.ducsai@gmail.com>
2023-12-03 22:36:29 +0100

    feat: home page redesign

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,40 +1,14 @@
-import Card from '@/components/Card';
-
-import Logo from '@/assets/logo.svg';
-import AboutBusinessIcon from '@/assets/about-business.svg';
-import ContactIcon from '@/assets/contact.svg';
-import CraftingIcon from '@/assets/crafting.svg';
+import Link from 'next/link';
 
 export default function Splash() {
   return (
-    <div className="grid h-[calc(100vh-68px)] place-items-center content-center gap-4 bg-red-400 sm:h-[calc(100vh-72px)]">
-      <div className="container">
-        <Logo className="mx-auto hidden sm:block sm:w-[368px] mb-8" />
-        <div className=" hidden justify-evenly gap-8 sm:flex">
-          <Card className="p-12">
-            <AboutBusinessIcon className="h-full w-full" />
-          </Card>
-          <Card className="p-12">
-            <CraftingIcon className="h-full w-full" />
-          </Card>
-          <Card className="p-12">
-            <ContactIcon className="h-full w-full" />
-          </Card>
-        </div>
-        <div className="d-btm-nav sm:hidden">
-          <button>
-            <a href="#vallalkozas">
-              <AboutBusinessIcon className="h-5 w-5" />
-            </a>
-          </button>
-          <button className="active">
-            <CraftingIcon className="h-5 w-5" />
-          </button>
-          <button>
-            <ContactIcon className="h-5 w-5" />
-          </button>
-        </div>
-      </div>
+    <section id="hello" className="grid h-[calc(100vh-68px)] place-items-center sm:h-[calc(100vh-76px)]">
+    <div>
+      <h1 className="hidden text-center font-bold min-[400px]:block ">www.papirsarkany.hu</h1>
+      <Link href={'/sarkanyok'}>
+        <h2 className="text-wrap-balance text-center font-semibold">Papírsárkány árusítás 1984-óta.</h2>
+      </Link>
     </div>
+  </section>
   );
 }
