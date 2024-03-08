0c47633d37e9980329cef33e217e770575759e8a
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-03-08 14:01:08 +0100

    feat: inject truendo script only in prod

---
+++ src/components/TruendoScript.tsx
@@ -0,0 +1,22 @@
+import Script from 'next/script';
+
+/**
+ * Truendo is a Consent Management Platform.
+ * I use this provider to meet the GDPR requirements.
+ * It injects a cookie banner to my sites.
+ * @returns Truendo banner integration script if NODE_ENV is production
+ */
+export default function TruendoScript() {
+  if (process.env.NODE_ENV !== 'production') {
+    return;
+  }
+
+  return (
+    <Script
+      id="truendoAutoBlock"
+      type="text/javascript"
+      src="https://cdn.priv.center/pc/truendo_cmp.pid.js"
+      data-siteid="88306c5c-ce8c-45d7-8daf-47e99864c1b8"
+    />
+  );
+}
