3893baf0b9893bfa805525f4000bbf9b6b2c7bac
Bálint Ducsai <balint.ducsai@gmail.com>
2024-04-11 15:27:25 +0200

    refactor: remove sanity token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,12 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn, token } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
-  token,
   perspective: 'published',
 })
