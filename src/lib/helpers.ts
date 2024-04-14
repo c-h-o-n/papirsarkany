456aaad4f866e5f865106b6f4c846ae17653df76
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-04-14 20:55:59 +0200

    fix: differntiate cart items with different names

---
+++ src/lib/helpers.ts
@@ -0,0 +1,5 @@
+import { CartItem } from './types';
+
+export function isInCart(itemToCheck: CartItem, itemInCart: CartItem) {
+  return itemToCheck._id === itemInCart._id && itemToCheck.name === itemInCart.name
+}
