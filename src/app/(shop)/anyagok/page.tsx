7041465a34476638fc098bea2703a56a7bc64f2f
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-04-17 11:09:47 +0200

    feat: add background patterns

--- src/app/(shop)/anyagok/page.tsx
+++ src/app/(shop)/anyagok/page.tsx
@@ -12,42 +12,40 @@ export default async function Materials() {
   const twines = await getAllTwines();
 
   return (
-    <div className="container space-y-8 p-8">
+    <div className="container space-y-10 p-8">
       <h1 className="text-center font-bold">Anyagok</h1>
-
-      {reels && reels.length > 0 && (
-        <div className="space-y-4">
-          <h2 className="font-bold">{SanityCategoryMap['reel']}</h2>
-          <MasonryContainer>
-            {reels.map((reel) => (
-              <ReelCard reel={reel} key={reel._id} />
-            ))}
-          </MasonryContainer>
-        </div>
-      )}
-
-      {twines && twines.length > 0 && (
-        <div className="space-y-4">
-          <h2 className="font-bold">{SanityCategoryMap['twine']}</h2>
-
-          <MasonryContainer>
-            {twines.map((twine) => (
-              <TwineCard twine={twine} key={twine._id} />
-            ))}
-          </MasonryContainer>
-        </div>
-      )}
-
-      {rods && rods.length > 0 && (
-        <div className="space-y-4">
-          <h2 className="font-bold">{SanityCategoryMap['rod']}</h2>
-          <MasonryContainer>
-            {rods.map((rod) => (
-              <RodCard rod={rod} key={rod._id} />
-            ))}
-          </MasonryContainer>
-        </div>
-      )}
+      <div className="space-y-8">
+        {reels && reels.length > 0 && (
+          <div className="space-y-4">
+            <h2 className="font-bold">{SanityCategoryMap['reel']}</h2>
+            <MasonryContainer>
+              {reels.map((reel) => (
+                <ReelCard reel={reel} key={reel._id} />
+              ))}
+            </MasonryContainer>
+          </div>
+        )}
+        {twines && twines.length > 0 && (
+          <div className="space-y-4">
+            <h2 className="font-bold">{SanityCategoryMap['twine']}</h2>
+            <MasonryContainer>
+              {twines.map((twine) => (
+                <TwineCard twine={twine} key={twine._id} />
+              ))}
+            </MasonryContainer>
+          </div>
+        )}
+        {rods && rods.length > 0 && (
+          <div className="space-y-4">
+            <h2 className="font-bold">{SanityCategoryMap['rod']}</h2>
+            <MasonryContainer>
+              {rods.map((rod) => (
+                <RodCard rod={rod} key={rod._id} />
+              ))}
+            </MasonryContainer>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/app/(shop)/sarkanyok/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+export default function KiteLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <div className='kite-pattern'>
+      {children}
+    </div>
+  )
+}
