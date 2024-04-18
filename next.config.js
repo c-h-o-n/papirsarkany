b389dd38070b8a00421ef6082ea9e6d154f762be
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-04-18 23:16:14 +0200

    chore: disallow /admin routes to be crawled

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ function getAppVersion() {
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
