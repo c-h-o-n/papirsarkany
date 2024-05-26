19ed5024cce1263a2fec6c4b002783678b35671d
Bálint Ducsai <balint.ducsai@gmail.com>
2024-05-26 21:05:53 +0200

    refactor(order): move route to /api/order

--- src/app/(shop)/api/order/route.ts
+++ src/app/(shop)/api/order/route.ts
@@ -55,8 +55,8 @@ export async function POST(request: Request) {
       await sendEmail({
         from: 'mail@papirsarkany.hu',
         to: 'balint.ducsai@gmail.com',
-        subject: 'error in papirsarkany.hu/api',
-        text: `Error caught in url papirsarkany/api. \nreason: ${error}`,
+        subject: 'error detected in papirsarkany.hu/api/order',
+        text: `Error caught in url papirsarkany/api/order. \nreason: ${error}`,
       });
     }
 
