14c0a21b4b88d50b67d00cd76542f6c38f2fc4ca
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-07-30 13:36:22 +0200

    refactor(cms): queries into separate file

--- src/lib/cms.ts
+++ src/lib/cms.ts
@@ -6,17 +6,21 @@ import {
   GetAllTwinesQueryResult,
   GetKiteBySlugQueryResult,
 } from '@sanity/lib/sanity.types';
-import { groq } from 'next-sanity';
+import {
+  getAllKitesQuery,
+  getKiteBySlugQuery,
+  getAllRodsQuery,
+  getAllReelsQuery,
+  getAllTwinesQuery,
+} from './queries';
 
 export async function getAllKites(): Promise<GetAllKitesQueryResult> {
-  const getAllKitesQuery = groq`*[_type == 'kite'] { ..., image { asset -> { url, metadata } } } | order(price asc)`;
   return await client.fetch<GetAllKitesQueryResult>(getAllKitesQuery);
 }
 
 export async function getKiteBySlug(
   slug: string,
 ): Promise<GetKiteBySlugQueryResult> {
-  const getKiteBySlugQuery = groq`*[_type == 'kite' && slug.current == $slug] { ..., image { asset-> { url, metadata } } }[0]`;
   return await client.fetch<GetKiteBySlugQueryResult, { slug: string }>(
     getKiteBySlugQuery,
     {
@@ -26,19 +30,13 @@ export async function getKiteBySlug(
 }
 
 export async function getAllRods() {
-  const getAllRodsQuery = groq`*[_type == 'rod'] { ..., image { asset-> { url, metadata } } } | order(name asc)`;
-
   return await client.fetch<GetAllRodsQueryResult>(getAllRodsQuery);
 }
 
 export async function getAllReels(): Promise<GetAllReelsQueryResult> {
-  const getAllReelsQuery = groq`*[_type == 'reel'] { ..., image { asset-> { url, metadata } } } | order(name asc)`;
-
   return await client.fetch(getAllReelsQuery);
 }
 
 export async function getAllTwines(): Promise<GetAllTwinesQueryResult> {
-  const getAllTwinesQuery = groq`*[_type == 'twine'] { ..., image { asset-> { url, metadata } } } | order(name asc)`;
-
   return await client.fetch(getAllTwinesQuery);
 }
