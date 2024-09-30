ea8abb430dea510652b49921a4b91f651d6f448a
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-09-30 18:42:10 +0200

    fix(zod): add error error message on missing package info object

--- src/lib/validation-schemas.ts
+++ src/lib/validation-schemas.ts
@@ -7,12 +7,17 @@ export const cartItemValidationSchema = z.object({
   _id: z.string().min(1, 'Hiányzó termék azonosító.'),
   name: z.string().min(1, 'Hiányzó név'),
   price: z.number().positive('Érvénytelen ár'),
-  packageInfo: z.object({
-    x: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
-    y: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
-    z: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
-    weight: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
-  }),
+  packageInfo: z.object(
+    {
+      x: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
+      y: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
+      z: z.number().positive('Hiányzó vagy érvénytelen csomag információk'),
+      weight: z
+        .number()
+        .positive('Hiányzó vagy érvénytelen csomag információk'),
+    },
+    { message: 'Hiányzó vagy érvénytelen csomag információk' },
+  ),
   quantity: z.number().positive('Érvénytelen mennyiség'),
 });
 
