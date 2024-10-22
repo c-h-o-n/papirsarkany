78746d13f53e77792d738be6643c077cefafb199
Bálint "chon" Ducsai <balint.ducsai@gmail.com>
2024-10-22 16:03:07 +0200

    test(helpers): group normalizeOrder form tests

--- src/tests/unit/helpers.spec.ts
+++ src/tests/unit/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, vi } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 
 import { LOCAL_PICKUP_ADDRESS } from '~/lib/constants';
 import { delay, normalizeOrderForm } from '~/lib/helpers';
@@ -24,57 +24,61 @@ test('delay - resolves after the given time', async () => {
   vi.useRealTimers();
 });
 
-test('normalizeOrderForm - set shipping information to undefined if "Személyes átvétel" is selected', () => {
-  const formData: OrderForm = {
-    firstName: 'János',
-    lastName: 'Kulka',
-    email: 'janos.kulka@gmail.com',
-    shippingOption: 'Személyes átvétel',
-    shippingPostcode: LOCAL_PICKUP_ADDRESS.shippingPostcode,
-    shippingCity: LOCAL_PICKUP_ADDRESS.shippingCity,
-    shippingAddress: LOCAL_PICKUP_ADDRESS.shippingAddress,
-    isSameAdressAsShipping: false,
-    paymentOption: 'Átvételkor készpénzel',
-    phoneNumber: '+36201234567',
-    billingPostcode: '1025',
-    billingCity: 'Budapest',
-    billingAddress: 'Felső Zöldmáli út 13.',
-    billingSubaddress: '3. ajtó',
-    comment: 'just a unit test',
-  };
-
-  const normalizedOrderFormData = normalizeOrderForm(formData);
-
-  expect(normalizedOrderFormData).toEqual({
-    ...formData,
-    shippingPostcode: undefined,
-    shippingCity: undefined,
-    shippingAddress: undefined,
-    shippingSubaddress: undefined,
+describe('normalizeOrderForm', () => {
+  test('set shipping information to undefined if "Személyes átvétel" is selected', () => {
+    const formData: OrderForm = {
+      firstName: 'János',
+      lastName: 'Kulka',
+      email: 'janos.kulka@gmail.com',
+      shippingOption: 'Személyes átvétel',
+      shippingPostcode: LOCAL_PICKUP_ADDRESS.shippingPostcode,
+      shippingCity: LOCAL_PICKUP_ADDRESS.shippingCity,
+      shippingAddress: LOCAL_PICKUP_ADDRESS.shippingAddress,
+      isSameAdressAsShipping: false,
+      paymentOption: 'Átvételkor készpénzel',
+      phoneNumber: '+36201234567',
+      billingPostcode: '1025',
+      billingCity: 'Budapest',
+      billingAddress: 'Felső Zöldmáli út 13.',
+      billingSubaddress: '3. ajtó',
+      comment: 'just a unit test',
+    };
+  
+    const normalizedOrderFormData = normalizeOrderForm(formData);
+  
+    expect(normalizedOrderFormData).toEqual({
+      ...formData,
+      shippingPostcode: undefined,
+      shippingCity: undefined,
+      shippingAddress: undefined,
+      shippingSubaddress: undefined,
+    });
   });
-});
+  
+  test('returns the same object if "Postai szállítás" is selected', () => {
+    const formData: OrderForm = {
+      firstName: 'János',
+      lastName: 'Kulka',
+      email: 'janos.kulka@gmail.com',
+      shippingOption: 'Postai szállítás',
+      shippingPostcode: '1025',
+      shippingCity: 'Budapest',
+      shippingAddress: 'Felső Zöldmáli út 13.',
+      shippingSubaddress: '3. ajtó',
+      isSameAdressAsShipping: true,
+      paymentOption: 'Átvételkor készpénzel',
+      phoneNumber: '+36201234567',
+      billingPostcode: '1025',
+      billingCity: 'Budapest',
+      billingAddress: 'Felső Zöldmáli út 13.',
+      billingSubaddress: '3. ajtó',
+      comment: 'just a unit test',
+    };
+  
+    const normalizedOrderFormData = normalizeOrderForm(formData);
+  
+    expect(normalizedOrderFormData).toBe(formData);
+  });
+})
 
-test('normalizeOrderForm - returns the same object if "Postai szállítás" is selected', () => {
-  const formData: OrderForm = {
-    firstName: 'János',
-    lastName: 'Kulka',
-    email: 'janos.kulka@gmail.com',
-    shippingOption: 'Postai szállítás',
-    shippingPostcode: '1025',
-    shippingCity: 'Budapest',
-    shippingAddress: 'Felső Zöldmáli út 13.',
-    shippingSubaddress: '3. ajtó',
-    isSameAdressAsShipping: true,
-    paymentOption: 'Átvételkor készpénzel',
-    phoneNumber: '+36201234567',
-    billingPostcode: '1025',
-    billingCity: 'Budapest',
-    billingAddress: 'Felső Zöldmáli út 13.',
-    billingSubaddress: '3. ajtó',
-    comment: 'just a unit test',
-  };
 
-  const normalizedOrderFormData = normalizeOrderForm(formData);
-
-  expect(normalizedOrderFormData).toBe(formData);
-});
