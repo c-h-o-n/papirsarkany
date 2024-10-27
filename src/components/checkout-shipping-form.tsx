8811aa7eef9cf0725376be92c8a53235ffc9baf1
Bálint Ducsai <balint.ducsai@gmail.com>
2024-10-27 21:00:38 +0100

    fix(order-form):
    layout wrapping in mobile

--- src/components/checkout-shipping-form.tsx
+++ src/components/checkout-shipping-form.tsx
@@ -250,7 +250,7 @@ const CheckoutShippingForm: FC = () => {
         <div className="flex justify-end">
           <button
             type="submit"
-            className="d-btn d-btn-primary uppercase max-sm:d-btn-block"
+            className="d-btn d-btn-primary uppercase d-btn-block sm:w-auto"
           >
             Tovább
           </button>
