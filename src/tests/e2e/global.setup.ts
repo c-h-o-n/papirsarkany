699559ed9dfbc4c9066c6b2d93ce17b0da2f5b35
Bálint Ducsai <balint.ducsai@gmail.com>
2024-10-31 20:20:36 +0100

    test: check storage state exists

--- src/tests/e2e/global.setup.ts
+++ src/tests/e2e/global.setup.ts
@@ -1,8 +1,19 @@
 import { test as setup } from '@playwright/test';
+import fs from 'node:fs';
+import path from 'node:path';
 
-setup('bypass cookie consent', async ({ page, baseURL }) => {
+function isStorageStateExists() {
+  return fs.existsSync(
+    path.resolve(
+      import.meta.dirname!,
+      '../../../playwright-storage-state.json',
+    ),
+  );
+}
+
+setup('accept cookie consent', async ({ page, baseURL }) => {
   setup.skip(
-    Boolean(baseURL?.includes('127.0.0.1')),
+    Boolean(baseURL?.includes('127.0.0.1')) && isStorageStateExists(),
     'Skipping cookie consent in local environment',
   );
 
