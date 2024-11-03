4a347d4cbcd5a0a61153e8c4ca572d3c22f4e7e1
Bálint Ducsai <balint.ducsai@gmail.com>
2024-11-03 22:22:50 +0100

    refactor(truendo): add strategy beforeInteractive to the script components

--- src/components/truendo-script.tsx
+++ src/components/truendo-script.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import Script from 'next/script';
 
 import { isProdEnv } from '~/lib/helpers';
 
 /**
+ * IMPORTANT: Import me only in the root layout.
+ * 
  * Truendo is a Consent Management Platform.
- * I use this provider to meet the GDPR requirements.
- * It injects a cookie banner to my sites.
+ * A provider to meet the GDPR requirements, it injects a cookie banner.
  * @returns Truendo banner integration script if NODE_ENV and VERCEL_ENV is production
  */
 const TruendoScript = () => {
@@ -16,6 +18,7 @@ const TruendoScript = () => {
   return (
     <Script
       id="truendoAutoBlock"
+      strategy="beforeInteractive"
       type="text/javascript"
       src="https://cdn.priv.center/pc/truendo_cmp.pid.js"
       data-siteid="88306c5c-ce8c-45d7-8daf-47e99864c1b8"
