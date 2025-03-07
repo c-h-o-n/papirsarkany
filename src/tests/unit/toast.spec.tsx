7afc168e7ead0fffa85a0e2149ea96f3ab9486a9
Bálint Ducsai <balint.ducsai@gmail.com>
2025-03-07 11:57:17 +0100

    test(toast): add test for programmatical render

--- src/tests/unit/toast.spec.tsx
+++ src/tests/unit/toast.spec.tsx
@@ -1,7 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, renderHook, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import Toast from "~/components/toast";
 import type { Toast as ToastProps } from "~/lib/types";
+import { useToastStore } from "~/store/use-toast-store";
+import ToastContainer from "../../components/toast-container";
 
 test("Error Toast should match snapshot", () => {
   const errorToast: ToastProps = {
@@ -25,4 +27,23 @@ test("Success Toast should match snapshot", () => {
   const { container } = render(<Toast toast={successToast} />);
 
   expect(container).toMatchSnapshot();
+});
+
+test("Toast container should render toasts programmatically", async () => {
+  const toast: ToastProps = {
+    id: "success-toast-test",
+    type: "error",
+    message: "Hurray! You are awesome!",
+  };
+
+  render(<ToastContainer />);
+
+  const { result } = renderHook(() => useToastStore((state) => state.toast));
+
+  result.current(toast);
+
+  expect(await screen.findByText(toast.message)).toBeDefined();
+  expect((await screen.findByText(toast.message)).textContent).toBe(
+    "Hurray! You are awesome!",
+  );
 });
