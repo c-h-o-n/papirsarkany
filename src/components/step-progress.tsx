7b71743a467caacd12c2454be86635dc2aa578ae
Bálint Ducsai <balint.ducsai@gmail.com>
2025-03-30 13:53:36 +0200

    feat(step-progress): add success styles on laststep reached

--- src/components/step-progress.tsx
+++ src/components/step-progress.tsx
@@ -7,6 +7,8 @@ const StepProgress: FC = () => {
   const step = useCheckoutFormStore((state) => state.step);
   const setStep = useCheckoutFormStore((state) => state.setStep);
 
+const isLastStepReached = step === STEP_LABELS.length - 1;
+
   const onStepClick = (isPrevious: boolean, stepToSet: number) => {
     if (!isPrevious) {
       return;
@@ -16,7 +18,7 @@ const StepProgress: FC = () => {
 
   return (
     <div className="overflow-x-auto">
-      <ul className="d-steps w-full gap-2">
+      <ul className="d-steps d-step-primary w-full gap-2">
         {STEP_LABELS.map((stepLabel, idx) => {
           const isActive = step >= idx;
           const isPrevious = step > idx;
@@ -25,7 +27,7 @@ const StepProgress: FC = () => {
               tabIndex={isPrevious ? 0 : -1}
               type="button"
               key={stepLabel}
-              className={`d-step font-semibold after:content-[counter(step)]! focus-within:after:outline focus-visible:outline-hidden focus-visible:after:outline-neutral focus-visible:after:outline-offset-2 ${isActive ? "d-step-info" : "before:bg-white! after:bg-white!"} ${isPrevious ? "cursor-pointer" : ""}`}
+              className={`d-step font-semibold before:transition-all before:duration-300 after:transition-all after:duration-300 after:content-[counter(step)]! focus-within:after:outline focus-visible:outline-hidden focus-visible:after:outline-neutral focus-visible:after:outline-offset-2 ${isActive ? "d-step-info" : "before:bg-white! after:bg-white!"} ${isPrevious ? "cursor-pointer" : ""} ${isLastStepReached ? "d-step-success": ""}`}
               onClick={() => onStepClick(isPrevious, idx)}
             >
               {stepLabel}
