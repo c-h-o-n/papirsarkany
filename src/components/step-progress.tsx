a13eab9ccd8209759978fc8f489406cb8354301e
Bálint Ducsai <balint.ducsai@gmail.com>
2025-03-30 15:07:35 +0200

    style: run format

--- src/components/step-progress.tsx
+++ src/components/step-progress.tsx
@@ -7,7 +7,7 @@ const StepProgress: FC = () => {
   const step = useCheckoutFormStore((state) => state.step);
   const setStep = useCheckoutFormStore((state) => state.setStep);
 
-const isLastStepReached = step === STEP_LABELS.length - 1;
+  const isLastStepReached = step === STEP_LABELS.length - 1;
 
   const onStepClick = (isPrevious: boolean, stepToSet: number) => {
     if (!isPrevious) {
@@ -27,7 +27,7 @@ const isLastStepReached = step === STEP_LABELS.length - 1;
               tabIndex={isPrevious ? 0 : -1}
               type="button"
               key={stepLabel}
-              className={`d-step font-semibold before:transition-all before:duration-300 after:transition-all after:duration-300 after:content-[counter(step)]! focus-within:after:outline focus-visible:outline-hidden focus-visible:after:outline-neutral focus-visible:after:outline-offset-2 ${isActive ? "d-step-info" : "before:bg-white! after:bg-white!"} ${isPrevious ? "cursor-pointer" : ""} ${isLastStepReached ? "d-step-success": ""}`}
+              className={`d-step font-semibold before:transition-all before:duration-300 after:transition-all after:duration-300 after:content-[counter(step)]! focus-within:after:outline focus-visible:outline-hidden focus-visible:after:outline-neutral focus-visible:after:outline-offset-2 ${isActive ? "d-step-info" : "before:bg-white! after:bg-white!"} ${isPrevious ? "cursor-pointer" : ""} ${isLastStepReached ? "d-step-success" : ""}`}
               onClick={() => onStepClick(isPrevious, idx)}
             >
               {stepLabel}
